221b1062c9776e0f5a233f246840051d2a6df9c1
Oliver Webb (DHCW - Single Record) <oliver.webb@wales.nhs.uk>
2025-03-17 16:29:43 +0000

    Added front-end testing project

---
+++ Front-End_Automation_Framework/Front-End_Automation_Framework/Demo_Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Front_End_Automation_Framework
+{
+    internal class Demo_Library
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
+        public Demo_Library(IWebDriver driver)
+        {
+            this.driver = driver ?? throw new ArgumentNullException(nameof(driver));
+            this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+        public void searchBoxExample(string inputString)
+        {
+            Thread.Sleep(2000);
+            IWebElement inputBox = wait.Until(driver => driver.FindElement(/*ENTER IDENTIFIER HERE*/)));   //  <---- E.g.   driver.FindElement(By.Id("searchBox"));
+
+            //This sends an input is a search box is selected
+            inputBox.SendKeys(inputString);
+        }
+        public void clickButtonExample()
+        {
+            IWebElement button = wait.Until(driver => driver.FindElement(By.Id("exampleButton")));  //Change identifier as needed
+
+            //This clicks on the selected element
+            button.Click();
+        }
+    }
+}
---
+++ Front-End_Automation_Framework/Front-End_Automation_Framework/Template.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Front_End_Automation_Framework
+{
+    internal class Template
+    {
+        private IWebDriver driver;  // <-- Ignore this error
+        //Replace with your library
+        private Demo_Library library;
+        //SET BASE URL
+        private const string BaseUrl = "";  //<-- Enter desired base URL here
+
+        [SetUp]
+        public void Setup()
+        {
+            //This driver can be altered Eg. Firefox or Safari
+            driver = new ChromeDriver();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(BaseUrl);
+
+            //Replace with a real Library
+            library = new Demo_Library(driver);
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+            Console.WriteLine("Browser closed.");
+        }
+
+
+        [Test]
+        public void TestName()  // <-- Change name of test here
+        {
+            //Arrange 
+
+            /*      Outline the desired inputs and outputs of the test here    */
+
+            //Act
+
+            /*      Follow the user workflow here                   */
+
+            //Assert 
+
+            /*      Put the final test assertions here  Eg.  Assert.isTrue()    */
+        }
+
+        //Example of using the Library:
+        //This test will fail as the library won't identify any elements
+        [Test]
+        public void ExampleTest() 
+        {
+            //Arrange
+            string nameToBeSearched = "George";
+
+            //Act
+            library.searchBoxExample(nameToBeSearched);
+            library.clickButtonExample();
+
+            //Assert
+            Assert.IsTrue(1 == 1);    //  <-- Change this assertion as necessary
+
+
+        }
+    }
+}
